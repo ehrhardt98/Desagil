cb3b84246dbe4b1f5afbe31798812755ceb73b90
ehrhardt98 <jorgeavbe@al.insper.edu.br>
2018-02-28 12:13:18 -0300

    Add files via upload

---
+++ JorgeEhrhardt/src/main/java/br/pro/hashi/ensino/desagil/tequilada/Incremento2.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+public class Incremento2 {
+	
+    public static void main(String[] args) {
+    	
+        try (BufferedReader br = new BufferedReader(new FileReader("labirinto.txt"))) {
+        	int x,y;
+            String line,line0;
+            String[] coord;
+            line0 = br.readLine();
+            y = Integer.parseInt(line0.split(" ")[0]);
+            x = Integer.parseInt(line0.split(" ")[1]);
+            
+            
+            while ((line = br.readLine()) != null) {
+                System.out.println(line.replaceAll("#", "X"));
+            }
+            
+        } catch (IOException e) {
+        	
+            e.printStackTrace();
+        }
+        
+    }
+}
